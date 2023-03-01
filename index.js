0aa9faf38eb2e5b6e4495aaf310a63cf6dd4024a
chrisgom113 <cgomez4@uci.edu>
2023-03-01 15:51:15 -0800

    added questions, tested command line functionality

--- index.js
+++ index.js
@@ -3,6 +3,58 @@ const fs = require('fs');
 const path = require('path');
 const generateMarkdown = require('./utils/generateMarkdown');
 
+const questions = [
+    { 
+        type: "input",
+        name: "title",
+        message: "What's the title of your project?"
+
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Describe your project"
+    },
+    {
+        type: "input",
+        name: "installation instructions",
+        message: "what dependencies need to be installed to run your project?",
+        default: "npm i"
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "Describe the detailed usage of your project"  
+    },
+    {
+        type: "input",
+        name: "contributors",
+        message: "who are the contributors on this project?"
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "what command will run a test?",
+        default: "npm test"
+    },
+    {
+        type: "input",
+        name: "license",
+        message: "Which license does your project have?",
+        choices: ["MIT", "Open Software License 3.0", "The Unlicense", "ISC", "Mozilla Public License 2.0", "None"]
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "what is your Github username?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "what is your email address?"
+    }
+];
+
 function writeToFile(fileName, data) {
     return fs.writeFileSync(path.join(process.cwd(), fileName), data);
 }
