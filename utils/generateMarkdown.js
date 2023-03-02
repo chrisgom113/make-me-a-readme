e67cfc7bd2146ac17f9ad3531b1bd5c14c18a3cd
chrisgom113 <cgomez4@uci.edu>
2023-03-01 16:45:59 -0800

    added code for license badge function and added sections to readme

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,10 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+
+function renderLicenseBadge(license) {
+    if (license !== "None") {
+        return `![GitHub license](https://img.shields.io/badge/license-${license}-yellowgreen.svg)`
+      }
+      return ''
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -14,6 +18,25 @@ function renderLicenseSection(license) {}
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ${renderLicenseBadge(data.license)}
+
+  ## Description
+
+  ${data.description}
+
+  ## Table of Contents
+
+  ## Installation
+
+  ## Usage
+
+  ## License
+
+  ## Contributing
+
+  ## Tests
+
+  ## Questions
 `;
 }
 
